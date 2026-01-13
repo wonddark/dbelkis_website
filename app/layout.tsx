ae1a5361cd0912e4a3d45ef57aad1e4e84849431
Osmanys Fuentes L <ossmanys@gmail.com>
2026-01-13 10:01:18 +0400

    Update global fonts to use Inter and Sansation instead of Geist fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import {Inter, Sansation} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const sansation = Sansation({
+  variable: "--font-sansation",
+    weight: '700'
 });
 
 export const metadata: Metadata = {
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} ${sansation.variable} antialiased font-body`}
       >
         {children}
       </body>
