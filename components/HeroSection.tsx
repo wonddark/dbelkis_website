f20946b24da9fc2313e0893d40635b97a9a0adbc
Osmanys Fuentes L <ossmanys@gmail.com>
2026-01-14 10:07:06 +0400

    Refine HeroSection layout with responsive spacing updates and header alignment improvements

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -5,31 +5,33 @@ import Link from "next/dist/client/link";
 function HeroSection() {
   return (
     <section className="bg-[url(/images/hero.jpg)] bg-cover bg-center">
-      <div className="flex flex-col gap-14 bg-white/40 px-4 py-4">
-        <header className="mb-16 flex items-center justify-center">
-          <Image
-            src="/images/dbelkis-logo.jpeg"
-            alt="D'Belkis Cleaning Company"
-            width={400}
-            height={300}
-            className="aspect-square size-8"
-          />
-        </header>
-        <div className="flex flex-col items-center gap-2">
-          <div className="rounded-full border border-accent bg-paper px-2 py-1 text-[10px] text-body2">
-            5<span className="text-accent">★</span> Rated Cleaning Service
+      <div className="flex flex-col gap-14 bg-white/40 p-4 md:gap-8 md:p-6">
+        <div className="flex flex-col gap-16">
+          <header className="flex justify-center md:justify-start">
+            <Image
+              src="/images/dbelkis-logo.jpeg"
+              alt="D'Belkis Cleaning Company"
+              width={400}
+              height={300}
+              className="aspect-square size-9 md:size-12"
+            />
+          </header>
+          <div className="flex flex-col items-center gap-2">
+            <div className="rounded-full border border-accent bg-paper px-2 py-1 text-[10px] text-body2">
+              5<span className="text-accent">★</span> Rated Cleaning Service
+            </div>
+            <h1 className="text-center font-display text-4xl">
+              Professional Cleaning Services{" "}
+              <span className="text-accent">You Can Trust</span>
+            </h1>
+            <p className="text-center text-sm">
+              Reliable, high-quality cleaning for homes and businesses. Trained
+              staff, eco-friendly products, and spotless results every time.
+            </p>
           </div>
-          <h1 className="text-center font-display text-[36px]">
-            Professional Cleaning Services{" "}
-            <span className="text-accent">You Can Trust</span>
-          </h1>
-          <p className="text-center text-sm">
-            Reliable, high-quality cleaning for homes and businesses. Trained
-            staff, eco-friendly products, and spotless results every time.
-          </p>
         </div>
         <div className="flex flex-col gap-3">
-          <div className="5 flex flex-col gap-1">
+          <div className="flex flex-col gap-1.5">
             <Link
               href="/"
               className="rounded-full bg-primary px-6 py-3 text-center text-sm font-medium text-primary-contrast"
