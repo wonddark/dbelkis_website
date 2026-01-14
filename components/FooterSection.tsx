6541c08bd80ef74e87949e0e84ff1532d7f53b9a
Osmanys Fuentes L <ossmanys@gmail.com>
2026-01-14 11:21:23 +0400

    Refine FooterSection for enhanced responsiveness and spacing; simplify ServicesSection import formatting.

--- components/FooterSection.tsx
+++ components/FooterSection.tsx
@@ -12,19 +12,22 @@ function FooterSection() {
           alt="D'Belkis Cleaning Company"
           width={100}
           height={100}
-          className="size-9 self-center"
+          className="size-9 self-center object-cover md:size-12"
         />
-        <div className="flex flex-wrap justify-center gap-3 py-3">
-          <Link href="tel:+971527280736" className="text-sm text-body">
+        <div className="flex flex-wrap justify-center gap-3 py-3 md:-mt-2 md:gap-4">
+          <Link
+            href="tel:+971527280736"
+            className="text-sm text-body md:text-[15px]"
+          >
             +971527280736
           </Link>
           <Link
             href="mailto:dbelkissardinas@gmail.com"
-            className="text-sm text-body"
+            className="text-sm text-body md:text-[15px]"
           >
             dbelkissardinas@gmail.com
           </Link>
-          <address className="text-sm text-body">
+          <address className="text-sm text-body md:text-[15px]">
             SPC Sharjah Publishing City
           </address>
         </div>
