b5df8122912a802b0f12f46ecf688cbb328785fd
Osmanys Fuentes L <ossmanys@gmail.com>
2026-01-14 13:31:03 +0400

    Refactor ContactSection and FooterSection for improved responsiveness, spacing, and typography; add social media links to ContactSection; simplify ServicesSection FontAwesome import formatting.

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -2,34 +2,65 @@ import React from "react";
 import SectionContainer from "@/components/SectionContainer";
 import SectionTitle from "@/components/SectionTitle";
 import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSquareWhatsapp
+} from "@fortawesome/free-brands-svg-icons/faSquareWhatsapp";
+import {
+  faSquareInstagram
+} from "@fortawesome/free-brands-svg-icons/faSquareInstagram";
+import Link from "next/dist/client/link";
+import {
+  faSquareFacebook
+} from "@fortawesome/free-brands-svg-icons/faSquareFacebook";
 
 function ContactSection() {
   return (
     <SectionContainer>
       <SectionTitle title="Contact Us" />
-      <div className="flex flex-col gap-8 md:flex-row">
-        <form className="flex-auto">
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
+        <form className="w-full">
           <div className="flex flex-col gap-3">
+            <div className="flex items-center justify-center gap-6 md:pb-3 lg:pb-6">
+              <Link href="https://www.whatsapp.com">
+                <FontAwesomeIcon
+                  icon={faSquareWhatsapp}
+                  className="size-6 text-accent md:size-8 lg:size-10"
+                />
+              </Link>
+              <Link href="https://www.instagram.com">
+                <FontAwesomeIcon
+                  icon={faSquareInstagram}
+                  className="size-6 text-accent md:size-8 lg:size-10"
+                />
+              </Link>
+              <Link href="https://www.facebook.com">
+                <FontAwesomeIcon
+                  icon={faSquareFacebook}
+                  className="size-6 text-accent md:size-8 lg:size-10"
+                />
+              </Link>
+            </div>
             <input
               type="text"
               name="fullName"
               placeholder="Full Name"
-              className="rounded-lg border border-body2/25 bg-paper px-2 py-3 text-sm text-body placeholder:text-body2/70 focus-visible:border-0 focus-visible:ring-accent/25 focus-visible:outline-accent/25"
+              className="rounded-lg border border-body2/25 bg-paper px-2 py-3 text-sm text-body placeholder:text-body2/70 focus:border-0 focus:ring-2 focus-visible:border-0 lg:px-3 lg:py-4 lg:text-[15px]"
             />
             <input
               type="email"
               name="email"
               placeholder="Email Address"
-              className="rounded-lg border border-body2/25 bg-paper px-2 py-3 text-sm text-body placeholder:text-body2/70 focus-visible:border-0 focus-visible:ring-accent/25 focus-visible:outline-accent/25"
+              className="rounded-lg border border-body2/25 bg-paper px-2 py-3 text-sm text-body placeholder:text-body2/70 focus-visible:border-0 focus-visible:ring-accent/50 lg:px-3 lg:py-4 lg:text-[15px]"
             />
             <textarea
               name="message"
               placeholder="Message"
-              className="h-28 rounded-lg border border-body2/25 bg-paper px-2 py-3 text-sm text-body placeholder:text-body2/70 focus-visible:border-0 focus-visible:ring-accent/25 focus-visible:outline-accent/25 md:h-36"
+              className="h-28 rounded-lg border border-body2/25 bg-paper px-2 py-3 text-sm text-body placeholder:text-body2/70 focus-visible:border-0 focus-visible:ring-accent/50 md:h-36 lg:h-48 lg:px-3 lg:py-4 lg:text-[15px]"
             />
             <button
               type="submit"
-              className="rounded-full bg-primary px-6 py-3 text-[13px] font-medium text-primary-contrast md:text-[15px]"
+              className="rounded-full bg-primary px-6 py-3 text-[13px] font-medium text-primary-contrast lg:self-start lg:text-[15px]"
             >
               Send message
             </button>
@@ -40,7 +71,7 @@ function ContactSection() {
           alt="D'Belkis Cleaning Company"
           width={720}
           height={720}
-          className="max-w-88 rounded-xl object-cover"
+          className="h-full w-full rounded-xl object-cover"
         />
       </div>
     </SectionContainer>
--- components/FooterSection.tsx
+++ components/FooterSection.tsx
@@ -12,22 +12,22 @@ function FooterSection() {
           alt="D'Belkis Cleaning Company"
           width={100}
           height={100}
-          className="size-9 self-center object-cover md:size-12"
+          className="size-9 self-center object-cover md:size-12 lg:size-16"
         />
-        <div className="flex flex-wrap justify-center gap-3 py-3 md:-mt-2 md:gap-4">
+        <div className="flex flex-wrap justify-center gap-3 py-3 md:-mt-2 md:gap-4 lg:-mt-6 lg:gap-6">
           <Link
             href="tel:+971527280736"
-            className="text-sm text-body md:text-[15px]"
+            className="text-sm text-body md:text-[15px] lg:text-base"
           >
             +971527280736
           </Link>
           <Link
             href="mailto:dbelkissardinas@gmail.com"
-            className="text-sm text-body md:text-[15px]"
+            className="text-sm text-body md:text-[15px] lg:text-base"
           >
             dbelkissardinas@gmail.com
           </Link>
-          <address className="text-sm text-body md:text-[15px]">
+          <address className="text-sm text-body md:text-[15px] lg:text-base">
             SPC Sharjah Publishing City
           </address>
         </div>
