929591e98baa93f45afbb2e6991047bb9d8b8195
Osmanys Fuentes L <ossmanys@gmail.com>
2026-01-14 12:16:25 +0400

    Enhance ReviewsSection for improved responsiveness in typography and spacing; refine Card component styling and simplify ServicesSection import formatting.

--- components/Card.tsx
+++ components/Card.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 function Card(props: Readonly<{ children: ReactNode }>) {
   return (
-    <div className="flex w-60 shrink-0 flex-col gap-4 rounded-2xl bg-paper p-3 shadow-[0_2px_4px_0] shadow-[rgb(107_114_128/0.1)]">
+    <div className="flex w-60 shrink-0 flex-col gap-4 rounded-2xl bg-paper p-3 shadow-[0_2px_4px_0] shadow-[rgb(107_114_128/0.1)] lg:w-80 lg:p-4">
       {props.children}
     </div>
   );
--- components/ReviewsSection.tsx
+++ components/ReviewsSection.tsx
@@ -50,19 +50,21 @@ function ReviewsSection() {
   return (
     <SectionContainer>
       <SectionTitle title="A Customer Success Story" />
-      <div className="flex gap-3 overflow-x-auto py-1">
+      <div className="flex justify-center gap-3 overflow-x-auto py-1 lg:gap-6">
         {reviews.map((review) => (
           <Card key={review.id}>
             <div className="flex flex-col items-center gap-1">
               <div className="flex flex-col items-center gap-0.5">
-                <div className="size-6 rounded-full bg-green-600 md:size-7" />
-                <span className="text-[10px] text-body2 md:text-xs">
+                <div className="size-6 rounded-full bg-green-600 md:size-7 lg:size-8" />
+                <span className="text-[10px] text-body2 md:text-xs lg:text-sm">
                   {review.author} - {review.date}
                 </span>
               </div>
-              <span className="text-xs text-accent md:text-sm">★★★★★</span>
+              <span className="text-xs text-accent md:text-sm lg:text-base">
+                ★★★★★
+              </span>
             </div>
-            <p className="line-clamp-4 text-[13px] italic md:text-sm">
+            <p className="line-clamp-4 text-[13px] italic md:text-sm lg:text-base">
               {review.content}
             </p>
           </Card>
